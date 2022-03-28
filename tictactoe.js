88a23349fa19821b577f41ee4dcd11617e700f4c
neoconstel <neoconstel@gmail.com>
2022-03-28 21:26:16 +0100

    Player module pattern created, tucking in CreatePlayer factory

--- tictactoe.js
+++ tictactoe.js
@@ -70,4 +70,46 @@ let Gameboard = (function() {
 })();
 
 
+let Player = (function() {
+    let numPlayers = 0;
+    let teams = [];
+
+    function CreatePlayer(team, isHuman) {
+        player = Object.create(null);
+        player.id = ++numPlayers;
+        player.isHuman = isHuman;
+        player.team = team;
+        if (!teams.includes(team))
+            teams.push(team);
+
+        return player;
+    }
+
+    function getNumPlayers() {
+        return numPlayers;
+    }
+
+    function getNumTeams() {
+        return teams.length;
+    }
+
+    return {
+        CreatePlayer,
+        getNumPlayers,
+        getNumTeams,
+    };
+
+})();
+
+
 Gameboard.init(gridSize = 3);
+let p1 = Player.CreatePlayer(1, true);
+let p2 = Player.CreatePlayer(2, false);
+let p3 = Player.CreatePlayer(2, false);
+
+console.log("Player 1 id: " + p1.id);
+console.log("Player 2 id: " + p2.id);
+console.log("Player1 is human? " + p1.isHuman);
+console.log("Player2 is human? " + p2.isHuman);
+console.log("Number of players: " + Player.getNumPlayers());
+console.log("Number of Teams: " + Player.getNumTeams());
